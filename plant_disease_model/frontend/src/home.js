f240119c0dcee3a1a9f29ad93002ec48c30d5f69
ChaseNaidoo <chasenaidoo9@gmail.com>
2024-02-08 14:53:28 +0200

    Changed logo

--- plant_disease_model/frontend/src/home.js
+++ plant_disease_model/frontend/src/home.js
@@ -214,7 +214,7 @@ export const ImageUpload = () => {
             InBloom: Plant Disease Detection
           </Typography>
           <div className={classes.grow} />
-          <Avatar src={cblogo}></Avatar>
+          <Avatar src={logo}></Avatar>
         </Toolbar>
       </AppBar>
       <Container maxWidth={false} className={classes.mainContainer} disableGutters={true}>
